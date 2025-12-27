43c959c14465a5df3cf85fc8487b1fc6eb6b72c8
Tamara <tamaradaye0gmail.com>
2025-12-27 21:58:07 +0100

    small things

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use rand::{distr::Alphanumeric, Rng};
 
 pub enum Command {
     Send,
@@ -27,31 +28,87 @@ impl Config {
             _ => {return Err("Please specify a command");}
         };
 
-        Ok(Config { command, files })
+        Ok(Config {command, files})
 
     }
 
+    pub fn generate_hash() -> String {
+        let hash: String = rand::rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+        hash
+    }
 }
 
 pub mod file_sharing{
     pub mod sender {
 
-        use crate::Config;
+        use crate::{Command, Config};
         use std::net::{Ipv4Addr, TcpListener, TcpStream};
         use std::io::{Read, Write};
         use std::fs::File;
+        use std::env;
+        use std::process;
         use std::thread;
 
-        pub fn send_files (config: Config) -> Result<_,_> {
-            let server = TcpListener::bind("0.0.0.0:8201").unwrap();
+        pub fn send_files() -> Result<bool,&'static str> {
+            let args: Vec<String> = env::args().collect();
+
+            let config = Config::new(&args).unwrap_or_else(|err| {
+                eprintln!("incorrect arguments specified: {}", err);
+                process::exit(1);
+            });
+
+            let config_password = Config::generate_hash();
+
+            if let Command::Send = config.command {
+                let server = TcpListener::bind("0.0.0.0:8201").unwrap();
+                println!("Awaiting receiver\n on receiving end enter {config_password}");
+                
+                match server.accept() {
+                    Ok((mut stream, _addr)) => {
+                        validate_client(&mut stream, &config_password)?;
+                    },
+                    Err(e) => {
+                        eprintln!("error encountered {e:?}")
+                    }
+                };
+            }
+
+            Ok(true)
         }
 
-        pub fn validate_client(mut stream: TcpStream) -> Result<bool, &'static str>{
+        pub fn validate_client(stream: &mut TcpStream, server_password: &String) -> Result<bool, &'static str>{
             let mut buffer = [0; 1024];
-            stream.read(&mut buffer);
-            let password = String::from_utf8_lossy(&buffer[..]);
+            let mut attempts = 3;
+            loop {
+                let n = stream.read(&mut buffer).unwrap();
+                let password = String::from_utf8_lossy(&buffer);
+
+                if password.trim() == *server_password {
+                    return Ok(true)
+                } else {
+                    attempts -= 1;
+                    let mut response_message: String = format!("incorrect password specified \nYou have {attempts} left");
+                    stream.write_all(response_message.as_bytes());
+                }
+
+                if attempts <= 0 {
+                    break;
+                }
+            }
+
+            Err("Invalid connection failed to provide password")
         }
+
+
     }
 
-    pub mod receive_files {}
+    pub mod receive_files {
+
+    }
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
+use rand::{Rng, distr::Alphanumeric};
+use std::{env, io};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::env;
 use std::process;
 
 fn main() {
@@ -11,24 +12,32 @@ fn main() {
 
     println!("server listening on 127.0.0.1:8080");
 
-    for stream in server.incoming() {
-        match stream {
-            Ok(stream) => {
-                std::thread::spawn(|| handle_client(stream));
-            }
-
-            Err(e) => {
-                eprintln!("Error {e} occured");
-            }
+    match server.accept() {
+        Ok((stream, addr)) => {
+            println!("{:?}", addr);
+            handle_client(stream);
+        },
+        Err(e) => {
+            eprintln!("Client terminated request {:?}", e)
         }
     }
 }
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).expect("Failed to stream");
-    let request = String::from_utf8_lossy(&buffer[..]);
-    println!("Received request \n{}", request);
-    let response = "Hello, Client!".as_bytes();
-    stream.write(response).expect("Couldn't write response");
+
+    loop {
+        let bytes_read = stream.read(&mut buffer).expect("Failed to stream");
+        if bytes_read == 0 {
+            println!("terminated connection");
+            break;
+        }
+        let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+        println!("Received {}", message);
+        let mut input = String::from("");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Couldn't receive user input");
+        stream.write_all(&input.trim().as_bytes());
+    }
 }
